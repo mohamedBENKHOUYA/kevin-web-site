13bddc9fa8ba0d5efa030bccf1d2477bc40773dc
unknown <--unset>
2022-08-18 19:43:18 +0200

    feat(part 4): finished

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 
-import './style.css';   
+import './style.css';  
+
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.onclick = function nfe(e) {
+  const isExpanded = primaryNav.toggleAttribute('data-visible');
+  navToggle.setAttribute('aria-expanded', isExpanded);
+}  
